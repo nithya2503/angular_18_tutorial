f0054407a8e70a2156c1c4f7882cb025cb5d6990
nithyasri <kattelanithyasri9100@gmail.com>
2024-09-28 20:47:11 -0400

    initial version

---
+++ src/app/components/roles/roles.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-roles',
+  standalone: true,
+  imports: [],
+  templateUrl: './roles.component.html',
+  styleUrl: './roles.component.css'
+})
+export class RolesComponent {
+  firstName: string = "Angular Tutorial";
+  angularVersion = "Version 18";
+  version: number =18;
+  isActive: boolean = false;
+  currentDate: Date = new Date();
+  // dt: string, number, boolean, date, object, array, null, undefined
+
+
+}
