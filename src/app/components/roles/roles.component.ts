0e58219729e95c8f1bff0801bb979d0bb0209b08
nithya1123 <[kattelanithyasri9248@gmail.com]>
2024-10-11 11:20:21 -0400

    basic_version

--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-roles',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
@@ -13,6 +15,16 @@ export class RolesComponent {
   version: number =18;
   isActive: boolean = false;
   currentDate: Date = new Date();
+  inputType : string = "button";
+  selectedState: string = 'YOU';
+
+  showWelcomeAlert(){
+    alert("welcome to angular 18 tutorial");
+    }
+showMessage(message: string){
+  alert("welcome to " + message + " training.")
+  }
+
   // dt: string, number, boolean, date, object, array, null, undefined
 
 
