f3ce768e8684a698695b3b01c410eb88803ee03a
nithya1123 <[kattelanithyasri9248@gmail.com]>
2025-02-04 16:53:00 -0500

    Implemented databinding - interpolateion, property binding, 2-way databinding and event binding

--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,19 +1,29 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-roles',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
 export class RolesComponent {
+  //ways to define a variable below
   firstName: string = "Angular Tutorial";
   angularVersion = "Version 18";
   version: number =18;
   isActive: boolean = false;
   currentDate: Date = new Date();
+  inputType: string = "radio";
+  selectedState: string  = '';
   // dt: string, number, boolean, date, object, array, null, undefined
+
+  showWelcomeAlert(){
+    alert("welcome to angular")
+    }
+  showMessage(message: string){
+    alert(message)}
 
 
 }
